6c22711c6289c5ce1aca1e38d9f8ad88522ecf11
vimariano <vincentmariano@hotmail.com>
2021-11-21 22:07:17 -0800

    updated read me

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# 03 JavaScript: Password Generator
+# JavaScript: Password Generator
 
-Create an application that generates a random password based on user-selected criteria. The app runs in the browser and features dynamically updated HTML and CSS powered by your JavaScript code. It has a clean and polished user interface and is responsive, which ensures that it adapts to multiple screen sizes.
-
-If you are unfamiliar with special characters, take a look at some [examples of special characters on the OWASP Foundation website](https://www.owasp.org/index.php/Password_special_characters).
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+I created an application that generates a random password based on acceptance criteria. This app is run in the browser and uses dynamically updated HTML and CSS powered by JavaScript code.
 
 ## Acceptance Criteria
 
@@ -31,19 +21,5 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
-
-The following image demonstrates the application functionality:
-
-![An app window with the label Password Generator, an input field labeled Your Secure Password, and a Generate Password button.](./Assets/03-javascript-homework-demo.png)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
-
+## MOCK-UP
+![img](images\pg-mockup.jpg)
--- script.js
+++ script.js
@@ -8,41 +8,39 @@ var charset = "";
 // generates password after selection
 function generatePassword () {
   var charLength = window.prompt("Choose password length no less than 8 and no more than 128!");
-  var retVal = "";
   if (charLength >= 8 && charLength <= 128){
     // prompt next window
     var charLower = window.confirm("Do you want lowercase letters?");
     var charUpper = window.confirm("Do you want uppercase letters?");
     var charNumbers = window.confirm("Do you want numbers?");
     var charSpecial = window.confirm("Do you want special characters?");
-
-    switch (true){
-        case charLower:
-          charset += lowerCase
-          console.log(charset)
-        case charUpper:
-          charset += upperCase
-          console.log(charset)
-        case charNumbers:
-          charset += numbers
-          console.log(charset)
-        case charSpecial:
-          charset += special
-          console.log(charset)
-        };
-
-        for (var i = 0, n = charset.length; i < charLength; ++i) {
-          
-          retVal += charset.charAt(Math.floor(Math.random() * n));
-      }
-      return retVal;
-  
   }  else{
     window.alert("Please enter valid number.")
   }
 
+  if (charLower){
+    charset += lowerCase;
+    console.log(charset);
+  }
+  if (charUpper){
+    charset += upperCase;
+    console.log(charset);
+  } 
+  if (charNumbers){
+    charset += numbers;
+    console.log(charset);
+  } 
+  if (charSpecial){
+    charset += special;
+    console.log(charset);
+  } 
+   retVal = "";
+ for (var i = 0, n = charset.length; i < charLength; ++i) {  
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+      }
+
+      return retVal;
   // generates password
-  debugger;
 }
 
 // Get references to the #generate element
@@ -53,11 +51,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  generatePassword();
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-
